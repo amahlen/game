cf69d75be7fd2e1c7c6567a93ce8bf0c7ec29ab7
Amahle Ngcombo <ngcomboamahle@gmail.com>
2019-09-30 15:11:10 +0200

    Updates

---
+++ precache-manifest.5967531ab77c8d70b78bdd6fd56cb53d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68ff1398e39aaee1393dcc4bd6a9e8f2",
+    "url": "/game/index.html"
+  },
+  {
+    "revision": "ea84f451fe833d197e44",
+    "url": "/game/static/css/main.07add939.chunk.css"
+  },
+  {
+    "revision": "54ca904e1242e4b13c07",
+    "url": "/game/static/js/2.f261e86d.chunk.js"
+  },
+  {
+    "revision": "ea84f451fe833d197e44",
+    "url": "/game/static/js/main.7351b64b.chunk.js"
+  },
+  {
+    "revision": "c3ae8f33d028f1ec78e9",
+    "url": "/game/static/js/runtime~main.79d8ac20.js"
+  }
+]);
